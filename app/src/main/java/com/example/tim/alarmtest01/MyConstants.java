c88d145b95aa7a81e4873bb1c1cd2121b2fa1dd4
Snoopie0897 <35850605+Snoopie0897@users.noreply.github.com>
2018-01-26 15:35:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tim/alarmtest01/MyConstants.java
@@ -0,0 +1,11 @@
+package com.example.tim.alarmtest01;
+
+/**
+ * Created by Tim on 07-Dec-17.
+ */
+
+public abstract class MyConstants {
+    public static final String HOUR = "time_hour";
+    public static final String MINUTE = "time_minute";
+    public static final String TIME_PICKER = "time_picker";
+}
